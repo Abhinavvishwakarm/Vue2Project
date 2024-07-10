c3b966a4aa8d64a2aa098f1d6927f361002faf7a
Abhinavvishwakarm <157105478+Abhinavvishwakarm@users.noreply.github.com>
2024-07-10 13:29:39 +0530

    Add files via upload
    Vue2 project setup Applications

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+      <v-card>
+
+        <h1>vuetify</h1>
+      </v-card>
+    <h1>This is an home page</h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+    },
+  }
+</script>
---
+++ src/views/IncDec.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-card class="mx-auto" max-width="400">
+          <v-card-title>
+            <h1>{{ count }}</h1>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn class="btnIncrement" @click="increment">Increment</v-btn>
+          </v-card-actions>
+           <v-card-actions>
+            <v-btn class="btnDecrement" @click="decrement">Decrement</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  computed: {
+    count() {
+      return this.$store.getters.getCount;
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.dispatch('increment');
+    },
+    decrement() {
+      this.$store.dispatch('decrement')
+    }
+  }
+};
+</script>
+
+<style>
+/* Add your styles here */
+.btnIncrement {
+    margin-right: 10px;
+    color: red;
+    font-size: x-large;
+}
+.btnDecrement {
+    margin-left: 10px;
+    color: blue;
+    font-size: x-large;
+}
+</style>
